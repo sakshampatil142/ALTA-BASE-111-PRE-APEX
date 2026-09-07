e15a9f69e67e75fe54147177cdac52aa9efa3838
sakshampatil142 <sakshampatil142@gmail.com>
2026-09-07 13:43:15 +0530

    Add files via upload

---
+++ APEXDay6.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class APEXDay6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int largest;
+        if (a >= b) {
+            if (a >= c) {
+                largest = a;
+            } else {
+                largest = c;
+            }
+        } else {
+            if (b >= c) {
+                largest = b;
+            } else {
+                largest = c;
+            }
+        }
+
+        System.out.println(largest);
+    }
+}
