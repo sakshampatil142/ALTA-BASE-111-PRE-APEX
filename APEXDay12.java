9dbe3bb96222a7b64914888060c0637b9f7ce4ea
sakshampatil142 <sakshampatil142@gmail.com>
2026-09-13 16:06:48 +0530

    Add files via upload

---
+++ APEXDay12.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class APEXDay12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+
+        switch (month) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Summer");
+                break;
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                System.out.println("Monsoon");
+                break;
+            case 10:
+            case 11:
+                System.out.println("Autumn");
+                break;
+            default:
+                System.out.println("Invalid Month");
+        }
+    }
+}
