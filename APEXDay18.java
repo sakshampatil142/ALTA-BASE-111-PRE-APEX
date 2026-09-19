00a065cc06e1a5f0498d57a34a884cbcc0dcc74b
sakshampatil142 <sakshampatil142@gmail.com>
2026-09-19 21:50:31 +0530

    Add files via upload

---
+++ APEXDay18.java
@@ -0,0 +1,13 @@
+public class APEXDay18 {
+    public static void main(String[] args) {
+        int num = 1;
+
+        while (true) {
+            if (num % 7 == 0) {
+                System.out.println(num);
+                break;
+            }
+            num++;
+        }
+    }
+}
